f08323df51362af4eb49c9eeb768ad51c144784c
SCWinter259 <catungthongkhi2002@gail.com>
2022-07-25 21:20:01 -0600

    wrote half of check collision function

--- test.py
+++ test.py
@@ -44,6 +44,10 @@ class Score:
         return self.score
 
     def change_score(self):
+        '''
+        checks for ball collision with the end of the screen
+        and change the score if any collision happened
+        '''
         pass
 
     def draw(self):
@@ -87,6 +91,9 @@ class Ball:
         self.radius = 5
         self.color = pygame.Color("white")
 
+    def get_radius(self):
+        return self.radius
+
     def get_ball_x(self):
         return self.ball_x
 
@@ -105,7 +112,7 @@ class Ball:
     def draw(self):
         pass
 
-def check_collision():
+def check_collision(paddle, ball):
     '''
     Checks whether the ball and either paddles have collided
     or not. Returns 1 or 2 as of which paddle collided with the ball.
@@ -117,7 +124,20 @@ def check_collision():
     # direction: ball_velocity_x < 0
     # position: ball_x - radius <= paddle1_left + paddle_width; 
     #           paddle1_top + height >= ball_y - radius >= paddle1_top
-
+    left = paddle.get_left()
+    top = paddle.get_top()
+    width = paddle.get_width()
+    height = paddle.get_height()
+    direction = ball.get_velocity_x()
+    ball_x = ball.get_ball_x()
+    ball_y = ball.get_ball_y()
+    radius = ball.get
+    if left == 30:
+        if ((direction < 0) and (ball_x - radius <= left + width) and
+            (top + height >= ball_y - radius) and (ball_y - radius >= top)):
+            return 1
+        else:
+            return 0
     # ball collided with right (second) paddle
-
-    # else
+    else:
+        pass
