69daf0cbf269b7503459a6c540ce9cf885a0b9a0
SCWinter259 <catungthongkhi2002@gail.com>
2022-07-27 11:02:25 -0600

    kind of done class score

--- Score.py
+++ Score.py
@@ -1,22 +1,31 @@
 import pygame
 
 class Score:
-    def __init__(self, screen, paddle):
+    def __init__(self, screen):
         self.screen = screen
-        self.score = 0
+        self.score1 = 0
+        self.score2 = 0
         self.color = pygame.Color("white")
-        self.paddle = paddle
         self.font = pygame.font.SysFont("", 50)
 
-    def get_score(self):
-        return self.score
+    def get_score_1(self):
+        return self.score1
 
-    def change_score(self):
+    def get_score_2(self):
+        return self.score2
+
+    def change_score(self, ball, screen_x):
         '''
         checks for ball collision with the end of the screen
         and change the score if any collision happened
         '''
-        pass
+        if ball.get_ball_x() - ball.get_radius() <= 0:
+            self.score1 = self.score1 + 1
+        elif ball.get_ball_x() + ball.get_radius() >= screen_x:
+            self.score2 = self.score2 + 1
 
     def draw(self):
-        pass
+        left_score = self.font.render(str(self.score1), True, self.color)
+        self.screen.blit(left_score, (0, 0))          
+        right_score = self.render(str(self.score2), True, self.color)
+        self.screen.blit(right_score, (465, 0))
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pygame
-import Functions
+from Functions import check_collision
 import Ball
 import Paddle
 import Score
@@ -37,19 +37,29 @@ class Game:
         paddle2_left = screen_x - 30 - paddle_width
 
         # set up and down button for each paddle
-        up1 = pygame.K_q
-        down1 = pygame.K_a
-        up2 = pygame.K_q
-        down2 = pygame.K_l
+        self.up1 = pygame.K_q
+        self.down1 = pygame.K_a
+        self.up2 = pygame.K_q
+        self.down2 = pygame.K_l
 
         self.ball = Ball(screen, screen_x, screen_y)
         self.paddle1 = Paddle(screen, paddle1_left, paddle_top)
         self.paddle2 = Paddle(screen, paddle2_left, paddle_top)
-        self.score1 = Score(screen, self.paddle1)
-        self.score2 = Score(screen, self.paddle2)
+        self.score1 = Score(screen)
+        self.score2 = Score(screen)
 
     def update(self):
-        pass
+        self.ball.move(self.paddle1, self.paddle2)
+        self.paddle1.move(self.up1, self.down1)
+        self.paddle2.move(self.up2, self.down2)
+
+    def draw(self):
+        self.screen.fill(self.background_color) 
+        self.update()
+        self.ball.draw()
+        self.paddle1.draw()
+        self.paddle2.draw()
+        pygame.display.flip()
 
     def play(self):
         pass
@@ -57,5 +67,4 @@ class Game:
     def handle_event(self):
         pass
 
-    def draw(self):
-        pass
+main()
